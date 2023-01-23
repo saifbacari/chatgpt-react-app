8d77f6067b213b393f73125b8a71ee39d773cf38
Saifoullah Bacari <saifbacari@gmail.com>
2023-01-23 23:08:39 +0100

    adding new items

--- src/AIOptions/index.jsx
+++ src/AIOptions/index.jsx
@@ -1,4 +1,50 @@
-const arrayItems = [{
+export const arrayItems = [{
     name: 'Q&A',
-    id: 'q&a'
-}]
+    id: 'q&a',
+    description : "Answer questions based on existing knowledge"
+},
+{
+    name: 'Grammar correction',
+    id: 'grammarCorrection',
+    description : "Corrects sentences into standard English"
+},
+{
+    name: 'Summarize for a 2nd grader',
+    id: 'summerForA2ndGrader',
+    description : "Translates difficult text into simpler concepts"
+},
+{
+    name: 'Natural language to OpenAI API',
+    id: 'naturalLanguageToOpenaiApi',
+    description : "Create code to call the Stripe API using natural language"
+},{
+    name: 'Text to command',
+    id: 'textToCommand',
+    description : "Translate text into programmatic commands"
+},
+{
+    name: 'SQL translate',
+    id: 'sqlTranslate',
+    description : "Translate natural language to SQL queries"
+},
+{
+    name: 'Explain code',
+    id: 'explainCode',
+    description : "Explain a complicated piece of code"
+},
+{
+    name: 'Factual answering',
+    id: 'factualAnswering',
+    description : "Guide the model towards factual answering",
+},    
+{
+    name: 'Interview questions',
+    id: 'intereviewQuestions',
+    description : "Create interview questions"
+},
+{
+    name: 'Recipe creator (eat at your own risk)',
+    id: 'recipeCreator',
+    description : "Create a recipe from a list of ingredients"
+}
+];
--- src/components/OptionSelection.jsx
+++ src/components/OptionSelection.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { arrayItems } from "../AIOptions";
 
 export default function OptionSelection(){
     return (
     <>
         <h1>React API App</h1>
+        <div className="grid-main">
+            {arrayItems.map((item) => {
+                return (<>
+                    <div className="grid-child">
+                        <h4>{item.name}</h4>
+                        <p>{item.description}</p>
+                    </div>
+                </>
+                );
+            }
+            )}
+        </div>
     </>
     )
 }
